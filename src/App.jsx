9bf979d34e42050beba5a8f71c80315b9e702653
Brandon Babineau <babineau1991@gmail.com>
2023-12-28 19:01:09 -0500

    signup component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
 import './App.css'
+import Authenticate from "./Components/Authenticate";
+import SignUpForm from "./Components/SignUpForm";
+export default function App() {
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+}
